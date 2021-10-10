19bd73edecdbfdb48d43c134b371200285e1ef21
unknown <51909454+abinash84@users.noreply.github.com>
2021-10-10 16:34:22 +0530

    Added q2

---
+++ DIP-A2-G4/q2.py
@@ -0,0 +1,75 @@
+
+from PIL import Image
+from decimal import Decimal, getcontext
+
+getcontext().prec = 50    # Setting Decimal precision upto 50 places
+
+
+def conv_list(list,columns):     # List into a 2-D matrix
+        result=[]               
+        start = 0
+        end = columns
+        rows=len(list)/columns
+        for i in range(int(rows)): 
+            result.append(list[start:end])
+            start +=columns
+            end += columns
+        return result
+
+
+
+
+
+
+def arithmetic(prob_matrix=None,N=10,message=None,image_path=None):    # ?unction to perform arithmetic coding
+    cumul_matrix={}
+    if (prob_matrix == None or message == None):   # If probability tablw or 1-D array to encode are not given
+        im = Image.open(image_path).convert('L')
+        pixel_map = im.load()
+        width, height = im.size
+        freq_matrix={}    
+        img_array=[]    # Flattened 1-D list of pixel values of image
+        for i in range(width):
+            for j in range(height):
+                value=pixel_map[i,j]
+                img_array.append(value)
+                if (value not in freq_matrix):
+                    freq_matrix[value]=1
+                else:
+                    freq_matrix[value]+=1
+        total_pixels= width*height
+        prob_matrix=[]
+        for i in sorted(freq_matrix):
+            prob_matrix.append([i,freq_matrix[i]/total_pixels])     # Probability values of intensities stored in a 2-D matrix format where 1st column is intensity value and second column is probability       
+        if message==None:         # If no message to encode is given, encode the image
+            message=img_array
+        
+    
+
+    sum=0
+    for i in range(len(prob_matrix)):
+        cumul_matrix[prob_matrix[i][0]]=[Decimal(sum),Decimal(sum+prob_matrix[i][1])]
+        sum=sum+prob_matrix[i][1]
+    msglen=len(message)
+    encoded=[]
+    for i in range(0,msglen,N):
+        initial=i
+        final=min(msglen,i+N)
+        start=Decimal(0.0) # Current range beginning
+        end=Decimal(1.0) # Current range ending
+        for j in range(initial,final):
+            char=message[j]
+            prev_start=start
+            length=end-start
+            start=prev_start+ length*cumul_matrix[char][0]
+            end=prev_start + length*cumul_matrix[char][1]
+        diff= end-start
+        decimal_places = abs(int(f'{diff:e}'.split('e')[-1]))
+        ans=round((start+end)/2,decimal_places)
+        encoded.append(ans)
+
+    return encoded,prob_matrix
+
+
+
+
---
+++ DIP-A2-G4/q2_test.py
@@ -0,0 +1,44 @@
+
+import pickle
+import argparse
+from q2 import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--prob",nargs='+',type=float,help='Probability matrix with 2 columns')
+parser.add_argument("--N",default=10,type=int,help='Number of symbols to be coded together')
+parser.add_argument("--msg",nargs='+',default=None,type=int,help='Message to be encoded')
+parser.add_argument("--image",default='q2_Image.jpg',type=str,help='Path to the image file')
+args= parser.parse_args()
+
+
+if args.prob != None:
+    prob_matrix=conv_list(args.prob,2)
+else:
+    prob_matrix=None
+
+if args.prob != None and args.msg!=None:
+    args.image=None
+
+
+print("Parameters used are as follows:\n\n","Probability matrix - ",prob_matrix,"\n","N - ",args.N,"\n","Message - ",args.msg,"\n","Image Path - ",args.image)
+Encoded,prob_matrix=arithmetic(prob_matrix=prob_matrix,N=args.N,message=args.msg,image_path=args.image)
+print("\n")
+
+
+
+with open('q2_Output.pickle', 'wb') as f:
+    pickle.dump((Encoded,prob_matrix), f)    # Saving Arithmetic emcoding and Probability table in q2_Output.pickle
+
+
+with open('q2_Output.pickle', 'rb') as f:
+    Encoded,prob_matrix=pickle.load(f)    # Loading Arithmetic emcoding and Probability table from q2_Output.pickle
+
+
+print('Encoding is as follows : \n')
+for i in Encoded:
+    print(i)
+
+print('\n\n')
+print('The Probability matrix used is : \n')
+for i in prob_matrix:
+    print('Symbol : {} , probability : {}'.format(i[0],i[1]))
